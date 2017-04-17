5776649269735a5f2c21d6d576c98569d9161057
clouetb <benoit.clouet@gmail.com>
2017-04-17 12:34:22 +0200

    Premier commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ws',
+      version='1.0',
+      description='just an app',
+      author='Benoît Clouet',
+      author_email='benoit.clouet@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['tornado'],
+      )
